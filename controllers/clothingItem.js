9c5f656e590b7cba33f7ca1c1576ee695af3abf9
Donald Johnson <djjohnson380@gmail.com>
2025-03-27 12:32:51 -0700

    Update on Project 12

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -94,8 +94,8 @@ const likeItem = (req, res) => {
           .send({ message: "Invalid data format for liking the item" });
       }
       return res
-        .status(NOT_FOUND_STATUS_CODE)
-        .send({ message: "Item not found" });
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
     });
 };
 
